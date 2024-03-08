2447f2f407a6f80674b6c1f62f18775c41af09ed
James Bomediano <james_bomediano@hotmail.com>
2024-03-08 15:25:48 +0800

    Minor Changes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 
 function App() {
-  return <div>
-    <BrowserRouter>
-    <Routes>
-      <Route path ="/" element ={<Home />} />
-    </Routes>
-    </BrowserRouter>
-  </div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
