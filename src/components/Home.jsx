81ad9bf74027cfcd46e2da872fc4b4a901a64034
James Bomediano <james_bomediano@hotmail.com>
2024-03-08 10:39:49 +0800

    Modified Home Component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 
+import Navbar from "./Navbar";
+import About from "./About";
+import Hero from "./Hero";
+import Education from "/Education";
+import Experience from "./Experience";
+import Contact from "./Contact";
+import Footer from "./Footer";
+
+
 const Home = () => {
   return (
-<div></div>
+<div>
+    <Navbar/>
+</div>
+<main>
+    <Hero/>
+    <About/>
+    <Education/>
+    <Experience/>
+    <Contact/>
+</main>
+<Footer/>
 );
-}
+};
 
 
 export default Home;
