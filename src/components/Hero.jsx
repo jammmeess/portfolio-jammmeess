729538c3aebbac567fc7b6df2921e67394454155
James Bomediano <james_bomediano@hotmail.com>
2024-03-08 15:26:18 +0800

    Updated Hero Banner

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import HeroBanner from "../img/HeroVideo.mp4";
 
 const Hero = () => {
-  return <div></div>;
+  return (
+    <section className="hero-section">
+      <div className="hero-image d-flex justify-content-end">
+        <video src={HeroBanner} autoPlay loop muted />
+      </div>
+      <div className="hero-text text-start">
+        <h1 className="fw-bolder display-1">Welcome to my Portfolio</h1>
+        <button type="button" className="btn btn-outline-light">
+          Download my Resume
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
