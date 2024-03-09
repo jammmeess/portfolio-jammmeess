0f059cbc8172f8e42e7a000f24c717748cc98e3f
James Bomediano <james_bomediano@hotmail.com>
2024-03-09 22:06:14 +0800

    Updated the Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,11 +4,15 @@ import HeroBanner from "../img/HeroVideo.mp4";
 const Hero = () => {
   return (
     <section className="hero-section">
+      <div className="overlay"></div>
       <div className="hero-image d-flex justify-content-end">
         <video src={HeroBanner} autoPlay loop muted />
       </div>
+
       <div className="hero-text text-start">
-        <h1 className="fw-bolder display-1">Welcome to my Portfolio</h1>
+        <h1 className="fw-bolder display-1 hero-title">
+          Welcome to my Portfolio
+        </h1>
         <button type="button" className="btn btn-outline-light">
           Download my Resume
         </button>
