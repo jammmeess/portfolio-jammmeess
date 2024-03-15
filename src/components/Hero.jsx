ea53a8ad77ec1a91454f7b4a33920be2dca3f6ed
James Bomediano <james_bomediano@hotmail.com>
2024-03-15 16:01:20 +0800

    Updated Hero Banner Mobile Responsiveness, Added Download Resume Function

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,62 @@
 import React from "react";
 import HeroBanner from "../img/HeroVideo.mp4";
+import HeroMobile from "../img/HeroMobile.mp4";
+import Resume from "../files/Bomediano_Resume.pdf";
 
 const Hero = () => {
   return (
     <section className="hero-section">
-      <div className="overlay"></div>
-      <div className="hero-image d-flex justify-content-end">
-        <video src={HeroBanner} autoPlay loop muted />
-      </div>
+      <div className="container">
+        <div className="d-none d-sm-block">
+          <div className="overlay"></div>
+          <div className="hero-image col-lg-12 d-flex justify-content-end">
+            <video src={HeroBanner} autoPlay loop muted />
+          </div>
 
-      <div className="hero-text text-start">
-        <h1 className="fw-bolder display-1 hero-title">Hi there, I am a</h1>
-        <h1 className="fw-bolder display-1 hero-title mb-5 higlight">
-          Web Developer
-        </h1>
-        <button type="button" className="btn btn-outline-light">
-          Download my Resume
-        </button>
+          <div className="hero-text text-start">
+            <h1 className="fw-bolder display-1 hero-title">Hi there, I am a</h1>
+            <h1 className="fw-bolder display-1 hero-title mb-5 higlight">
+              Web Developer
+            </h1>
+            <button type="button" className="btn btn-outline-light">
+              Download my Resume
+            </button>
+          </div>
+        </div>
+
+        {/* Phone Responsive */}
+        <div className="d-block d-sm-none">
+          <div className="row">
+            <div className="col-5">
+              <div className="overlay"></div>
+              <div className="hero-mobilebanner">
+                <video
+                  src={HeroMobile}
+                  autoplay
+                  loop
+                  muted
+                  className="img-fluid"
+                />
+              </div>
+              <div className="hero-mobiletext">
+                <h1 className="fw-bolder hero-mobile display-1">
+                  Hi there, I am a
+                </h1>
+                <h1 className="fw-bolder hero-mobile display-1 higlight">
+                  Web
+                </h1>
+                <h1 className="fw-bolder hero-mobile display-1 mb-5 higlight">
+                  Developer
+                </h1>
+                <a href={Resume} download>
+                  <button type="button" className="btn btn-outline-light">
+                    Download my Resume
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
