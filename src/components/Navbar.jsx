2fcaba08291383a6b3c432b0c81fcd81f643905c
James Bomediano <james_bomediano@hotmail.com>
2024-03-15 16:01:59 +0800

    Updated Navbar. Added Website Logo and Updated Mobile Responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import navIcon from "../img/menu.png";
+import Logo from "../img/portfoliologo.png";
 
 const Navbar = () => {
   document.onreadystatechange = function () {
@@ -17,43 +19,44 @@ const Navbar = () => {
       className="navbar fixed-top navbar-expand-lg navbar-dark text-white"
       id="nav"
     >
-      <div class="container-fluid">
-        <a class="navbar-brand mx-4 text-light" href="/">
-          Kristoffer James
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          <img src={Logo} className="logo me-3" />
         </a>
         <button
-          class="navbar-toggler "
-          type="button"
+          className="navbar-toggler"
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent"
           aria-expanded="false"
           aria-label="Toggle navigation"
-        ></button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link text-light  ms-2" href="#about">
+        >
+          <span class="navbar-toggler-icon text-white"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link text-light  ms-2" href="#about">
                 About Me
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link text-light  ms-2" href="#education">
+            <li className="nav-item">
+              <a className="nav-link text-light  ms-2" href="#education">
                 My Education
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link text-light  ms-2" href="#experience">
+            <li className="nav-item">
+              <a className="nav-link text-light  ms-2" href="#experience">
                 My Experience
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link text-light ms-2" href="#projects">
+            <li className="nav-item">
+              <a className="nav-link text-light ms-2" href="#projects">
                 My Projects
               </a>
             </li>
-            <li class=" nav-item">
-              <a class="nav-link text-light " href="#contact">
+            <li className=" nav-item">
+              <a className="nav-link text-light " href="#contact">
                 Contact Me
               </a>
             </li>
