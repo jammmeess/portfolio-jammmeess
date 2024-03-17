03e790e19164fde12375ded50790c4f30e870570
James Bomediano <james_bomediano@hotmail.com>
2024-03-17 11:38:11 +0800

    Experience Section Design Changes to make it look similar so a CSS code

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,25 +7,42 @@ const Experience = () => {
         <div className="row ">
           <div class="col-lg-5 pb-5 experiencetitle-section">
             <h3 class="ps-5 pt-5 mt-5 text-light display-5">
-              work.<span className="text-warning">experience( )</span>
+              work.<span className="text-warning">experiences( )</span>
             </h3>
-            <p className="text-secondary ps-5">my previous work experience_</p>
+            <p className="text-secondary ps-5">my previous work experiences_</p>
           </div>
           <div className="col-lg-6 work-section">
-            <div class="col-lg-12 pb-5 text-light">
-              <h4>De Castro Consulting</h4>
-              <h5>Web Developer Intern</h5>
-              <p>January 2024 - April 2024</p>
+            <div class="col-lg-12 pb-3 text-light">
+              <h4 className="pb-2">
+                <strong className="text-warning">
+                  .decastro-consulting ({" "}
+                </strong>
+              </h4>
+              <h5>Web Developer Intern;</h5>
+              <p>January 2024 - April 2024; </p>
+              <span>
+                <strong className="text-warning">)</strong>
+              </span>
             </div>
-            <div class="col-lg-12 pb-5 text-light">
-              <h4>Costa Cruises</h4>
-              <h5>Demi Chef De Partie</h5>
-              <p>March 2018- March 2023</p>
+            <div class="col-lg-12 pb-3 text-light">
+              <h4 className="pb-2">
+                <strong className="text-warning">.costa-cruises ( </strong>
+              </h4>
+              <h5>Demi Chef De Partie;</h5>
+              <p>March 2018 - March 2023; </p>
+              <span>
+                <strong className="text-warning">)</strong>
+              </span>
             </div>
-            <div class="col-lg-12 pb-5 text-light">
-              <h4>Ibiza Beach Club BGC</h4>
-              <h5>Commis Chef</h5>
-              <p>February 2017-October 2017</p>
+            <div class="col-lg-12 pb-3 text-light">
+              <h4 className="pb-2">
+                <strong className="text-warning">.ibizabeachclub-bgc ( </strong>
+              </h4>
+              <h5>Commis Chef;</h5>
+              <p>February 2017 - October 2017 ; </p>
+              <span>
+                <strong className="text-warning">)</strong>
+              </span>
             </div>
           </div>
         </div>
@@ -43,19 +60,34 @@ const Experience = () => {
         </div>
         <div className="col-12 workmobile-section">
           <div class="col-12 pb-4 text-light">
-            <h4>De Castro Consulting</h4>
-            <h5>Web Developer Intern</h5>
-            <p>January 2024 - April 2024</p>
+            <h4 className="pb-2">
+              <strong className="text-warning">.decastro-consulting ( </strong>
+            </h4>
+            <h5>Web Developer Intern;</h5>
+            <p>January 2024 - April 2024; </p>
+            <span>
+              <strong className="text-warning">)</strong>
+            </span>
           </div>
           <div class="col-12 pb-4 text-light">
-            <h4>Costa Cruises</h4>
-            <h5>Demi Chef De Partie</h5>
-            <p>March 2018- March 2023</p>
+            <h4 className="pb-2">
+              <strong className="text-warning">.costa-cruises ( </strong>
+            </h4>
+            <h5>Demi Chef De Partie;</h5>
+            <p>March 2018 - March 2023; </p>
+            <span>
+              <strong className="text-warning">)</strong>
+            </span>
           </div>
-          <div class="col-12 pb-4 text-light">
-            <h4>Ibiza Beach Club BGC</h4>
-            <h5>Commis Chef</h5>
-            <p>February 2017-October 2017</p>
+          <div class="col-12 text-light">
+            <h4 className="pb-2">
+              <strong className="text-warning">.ibizabeachclub-bgc ( </strong>
+            </h4>
+            <h5>Commis Chef;</h5>
+            <p>February 2017 - October 2017 ; </p>
+            <span>
+              <strong className="text-warning">)</strong>
+            </span>
           </div>
         </div>
       </div>
