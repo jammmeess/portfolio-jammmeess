c10c264eef713663434642a663d3442f3ca700eb
James Bomediano <james_bomediano@hotmail.com>
2024-04-03 18:19:27 +0800

    Updated Projects Section

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,132 +1,125 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import Engine from "../img/engine.png";
 import OrderXpress from "../img/orderxpress.png";
 import Library from "../img/library.png";
+import DeCastroConsulting from "../img/DCC.png";
+import DeCastroLawFirm from "../img/DCLF.png";
+
+const projectsData = [
+  {
+    title: "De Castro Law Firm",
+    imgSrc: DeCastroLawFirm,
+    liveLink: "https://decastroconsulting.ph/law-firm/",
+    techStack: "WordPress, Css",
+    githubLink: null,
+    extraInfo: "Internship | De Castro Consulting",
+  },
+  {
+    title: "De Castro Consulting",
+    imgSrc: DeCastroConsulting,
+    liveLink: "https://decastroconsulting.ph/",
+    techStack: "WordPress, Css",
+    githubLink: null,
+    extraInfo: "Internship | De Castro Consulting",
+  },
+  {
+    title: "Engine_ Games",
+    imgSrc: Engine,
+    liveLink: "/",
+    techStack: "Laravel, Html, Css, Javascript, PHP, Bootstrap, mySQL",
+    githubLink: "https://github.com/jammmeess/Engine",
+    extraInfo: "Best Capstone Award | KodeGo Ph",
+  },
+  {
+    title: "OrderXpress",
+    imgSrc: OrderXpress,
+    liveLink: "https://orderxpress.vercel.app",
+    techStack: "ReactJs, Html, Css, Sass, Javascript, Bootstrap",
+    githubLink: "https://github.com/caranayjamesmichael/orderxpress",
+    extraInfo: "Group Project | KodeGo Ph",
+  },
+  {
+    title: "Library",
+    imgSrc: Library,
+    liveLink: "https://jammmeess.github.io/library/",
+    techStack: "Html, Css, Bootstrap",
+    githubLink: "https://github.com/jammmeess/library",
+    extraInfo: "First Project | KodeGo Ph",
+  },
+];
 
 const Projects = () => {
-  useEffect(() => {
-    // Remove scrollbar when component mounts
-    document.body.style.overflow = "hidden";
-
-    return () => {
-      // Restore scrollbar when component unmounts
-      document.body.style.overflow = "auto";
-    };
-  }, []);
-
   return (
     <section className="project-section" id="projects">
       <div className="container project py-5">
         <div className="row">
           <div className="col-lg-12">
-            <h3 className="display-5 text-light">
-              my.<span className="text-warning mb-5">Projects( )</span>
+            <h3 className="display-3 text-light  d-flex justify-content-center">
+              <strong>
+                my.<span className="text-code ">Projects( )</span>
+              </strong>
             </h3>
+            <p className="text-secondary d-flex justify-content-center">
+              Projects that I have worked on_
+            </p>
           </div>
         </div>
 
         <div className="row mt-5">
-          <div className="col-lg-12">
-            {/* Engine_ Games */}
-            <article class="postcard dark blue">
-              <a class="postcard__img_link" href="#">
-                <img class="postcard__img" src={Engine} alt="Engine_ Games" />
-              </a>
-              <div class="postcard__text">
-                <h1 class="postcard__title blue">
-                  <a href="#">Engine_ Games</a>
-                </h1>
-                <div class="postcard__subtitle small">
-                  <i class="fas fa-calendar-alt mr-2"></i>Laravel, Html, Css,
-                  Javascript, PHP, Bootstrap, mySQL
-                </div>
-                <div class="postcard__bar"></div>
-                <div class="postcard__preview-txt">
-                  <p>
-                    Live Link: <a href="/">Engine_ Games</a>
-                  </p>
-                  <p>
-                    <a href="https://github.com/jammmeess/Engine">
-                      <i>Github | Engine_ Games</i>
+          {projectsData.map((project, index) => (
+            <div className="col-lg-6" key={index}>
+              <article
+                className={`postcard dark ${index % 2 === 0 ? "red" : "blue"}`}
+              >
+                <a
+                  className="postcard__img_link"
+                  href={project.liveLink}
+                  target="_blank"
+                >
+                  <img
+                    className="postcard__img"
+                    src={project.imgSrc}
+                    alt={project.title}
+                  />
+                </a>
+                <div className="postcard__text">
+                  <h1
+                    className={`postcard__title ${
+                      index % 2 === 0 ? "red" : "green"
+                    }`}
+                  >
+                    <a href={project.liveLink} target="_blank">
+                      {project.title}
                     </a>
-                  </p>
+                  </h1>
+                  <div className="postcard__subtitle small">
+                    <i className="fas fa-calendar-alt mr-2"></i>
+                    {project.techStack}
+                  </div>
+                  <div className="postcard__bar"></div>
+                  <div className="postcard__preview-txt">
+                    {project.githubLink && (
+                      <p>
+                        Git Repository:{" "}
+                        <a href={project.githubLink} target="_blank">
+                          <i>{project.title}</i>
+                        </a>
+                      </p>
+                    )}
+                    {project.extraInfo && (
+                      <ul className="postcard__tagbox">
+                        <li className="tag__item">
+                          <i className="fas fa-tag mr-2"></i>
+                          {project.extraInfo}
+                        </li>
+                      </ul>
+                    )}
+                  </div>
                 </div>
-                <ul class="postcard__tagbox">
-                  <li class="tag__item">
-                    <i class="fas fa-tag mr-2"></i>Best Capstone Award
-                  </li>
-                </ul>
-              </div>
-            </article>
-
-            {/* OrderXpress */}
-            <article class="postcard dark red">
-              <a class="postcard__img_link" href="#">
-                <img
-                  class="postcard__img"
-                  src={OrderXpress}
-                  alt="OrderXpress"
-                />
-              </a>
-              <div class="postcard__text">
-                <h1 class="postcard__title red">
-                  <a href="#">OrderXpress</a>
-                </h1>
-                <div class="postcard__subtitle small">
-                  <i class="fas fa-calendar-alt mr-2"></i>ReactJs, Html, Css,
-                  Sass, Javascript, Bootstrap
-                </div>
-                <div class="postcard__bar"></div>
-                <div class="postcard__preview-txt">
-                  <p>
-                    Live Link:{" "}
-                    <a href="orderxpress.vercel.app">
-                      <i>OrderXpress</i>
-                    </a>
-                  </p>
-
-                  <p>
-                    Git Repository:{" "}
-                    <a href="https://github.com/caranayjamesmichael/orderxpress">
-                      <i>Github | OrderXpress</i>
-                    </a>
-                  </p>
-                </div>
-              </div>
-            </article>
-
-            {/* Library */}
-            <article class="postcard dark green">
-              <a class="postcard__img_link" href="#">
-                <img class="postcard__img" src={Library} alt="Library" />
-              </a>
-              <div class="postcard__text">
-                <h1 class="postcard__title green">
-                  <a href="#">Library</a>
-                </h1>
-                <div class="postcard__subtitle small">
-                  <i class="fas fa-calendar-alt mr-2"></i>Html, Css, Javascript
-                </div>
-                <div class="postcard__bar"></div>
-                <div class="postcard__preview-txt">
-                  <p>
-                    Live Link:{" "}
-                    <a href=" https://jammmeess.github.io/library/">
-                      <i>Library</i>
-                    </a>
-                  </p>
-
-                  <p>
-                    Git Repository:{" "}
-                    <a href="https://github.com/jammmeess/library">
-                      <i>Github | Library</i>
-                    </a>
-                  </p>
-                </div>
-              </div>
-            </article>
-          </div>
+              </article>
+            </div>
+          ))}
         </div>
       </div>
     </section>
