0d7c5929bb2bc8991c229335ffa68ce36b4f927a
James Bomediano <james_bomediano@hotmail.com>
2024-04-03 18:18:59 +0800

    Minor Design Changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,8 +15,8 @@ const Hero = () => {
 
           <div className="hero-text text-start">
             <h1 className="fw-bolder display-1 hero-title">Hi there, I am a</h1>
-            <h1 className="fw-bolder display-1 hero-title mb-5 higlight">
-              Web Developer
+            <h1 className="fw-bolder display-1 hero-title mb-5">
+              <span className="highlight">Web Developer</span>
             </h1>
             <button type="button" className="button">
               Download my Resume
@@ -27,26 +27,24 @@ const Hero = () => {
         {/* Phone Responsive */}
         <div className="d-block d-sm-none">
           <div className="row">
-            <div className="col-5">
-              <div className="overlay-mobile"></div>
-              <div className="hero-mobilebanner">
-                {/* <video src={HeroMobile} loop muted className="img-fluid" /> */}
-              </div>
-              <div className="hero-mobiletext">
-                <h1 className="fw-bolder hero-mobile display-1">Hi there,</h1>
-                <h1 className="fw-bolder hero-mobile display-1">I am a</h1>
-                <h1 className="fw-bolder hero-mobile display-1 higlight">
-                  Web
-                </h1>
-                <h1 className="fw-bolder hero-mobile display-1 mb-5 higlight">
-                  Developer
-                </h1>
-                <a href={Resume} download>
-                  <button type="button" className="button">
-                    Download my Resume
-                  </button>
-                </a>
-              </div>
+            <div className="overlay-mobile"></div>
+            <div className="hero-mobilebanner">
+              {/* <video src={HeroMobile} loop muted className="img-fluid" /> */}
+            </div>
+            <div className="hero-mobiletext ms-3 pe-5">
+              <h1 className="fw-bolder hero-mobile display-1">Hi there,</h1>
+              <h1 className="fw-bolder hero-mobile display-1">I am a</h1>
+              <h1 className="fw-bolder hero-mobile display-1 ">
+                <span className="highlight">Web</span>
+              </h1>
+              <h1 className="fw-bolder hero-mobile display-1 mb-5 ">
+                <span className="highlight">Developer</span>
+              </h1>
+              <a href={Resume} download>
+                <button type="button" className="button">
+                  Download my Resume
+                </button>
+              </a>
             </div>
           </div>
         </div>
