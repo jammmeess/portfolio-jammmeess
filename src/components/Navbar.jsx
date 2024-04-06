651fdf48c4dd5ae47e77a68f5cb6a8cb892e472b
jammmeess <james_bomediano@hotmail.com>
2024-04-06 18:57:51 +0800

    Updated Navigation Icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,8 @@ const Navbar = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon text-white"></span>
+          {/* <span class="navbar-toggler-icon text-white"></span> */}
+          <img src={navIcon} className="Icon"/>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
